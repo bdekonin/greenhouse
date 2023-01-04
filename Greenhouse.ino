a7d31167e83cc4c509e668ad048207a235ac0ed4
Bob <60445103+bdekonin@users.noreply.github.com>
2023-01-04 19:45:20 +0100

    Update Greenhouse.ino

--- Greenhouse.ino
+++ Greenhouse.ino
@@ -1,64 +1,42 @@
-//www.elegoo.com
-//2018.10.25
-
-
-#include <dht_nonblocking.h>
-#define DHT_SENSOR_TYPE DHT_TYPE_11
-
-static const int DHT_SENSOR_PIN = 2;
-DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
+/* https://docs.espressif.com/projects/esp-at/en/latest/esp32/AT_Command_Set/HTTP_AT_Commands.html#http-at */
 
 
 
-/*
- * Initialize the serial port.
- */
-void setup( )
-{
-  Serial.begin( 9600);
+/* List all wifi   ---> 'AT+CWLAP' */
+/* Connect to Wifi ---> 'AT+CWJAP="TestingNetwork","tinkerbell"' */
+
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial wifiSerial(7, 6);
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+  wifiSerial.begin(9600);
+  // wifiSerial.print("AT+CWLAP");
+
+  delay(10000);
+  Serial.println("Starting");
 }
 
-
-
-/*
- * Poll for a measurement, keeping the state machine alive.  Returns
- * true if a measurement is available.
- */
-static bool measure_environment( float *temperature, float *humidity )
-{
-  static unsigned long measurement_timestamp = millis( );
-
-  /* Measure once every four seconds. */
-  if( millis( ) - measurement_timestamp > 3000ul )
-  {
-    if( dht_sensor.measure( temperature, humidity ) == true )
-    {
-      measurement_timestamp = millis( );
-      return( true );
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available()) {
+    Serial.println("information");
+    wifiSerial.println("AT+CWLAP");
+    // wifiSerial.println("");
+    while (Serial.available()) {
+      Serial.read();
+      // delay(3);
     }
+    // Serial.TX
   }
-
-  return( false );
-}
-
-
-
-/*
- * Main program loop.
- */
-void loop( )
-{
-  float temperature;
-  float humidity;
-
-  /* Measure temperature and humidity.  If the functions returns
-     true, then a measurement is available. */
-  if( measure_environment( &temperature, &humidity ) == true )
-  {
-    Serial.print( "T = " );
-    Serial.print( temperature, 1 );
-    Serial.print( " deg. C, H = " );
-    Serial.print( humidity, 1 );
-    Serial.println( "%" );
+  if (wifiSerial.available()) {
+    while (wifiSerial.available()) {
+      char c = wifiSerial.read();
+      Serial.print(c);
+    }
   }
 }
